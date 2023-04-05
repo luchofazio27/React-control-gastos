12e33a73f93c72dca7a8cf571fa6efaf8726f4a6
luchofazio27 <luchofazio@gmail.com>
2023-04-05 00:31:47 -0300

    Validando form y creacion de componente Mensaje

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,14 @@
-import { useState } from 'react'
-import Header from './components/Header'
+import { useState } from "react";
+import Header from "./components/Header";
 
 function App() {
+  const [presupuesto, setPresupuesto] = useState(0);
 
   return (
     <div>
-      <Header />
+      <Header presupuesto={presupuesto} setPresupuesto={setPresupuesto} />
     </div>
-  )
+  );
 }
 
-export default App
+export default App;
--- src/components/NuevoPresupuesto.jsx
+++ src/components/NuevoPresupuesto.jsx
@@ -1,10 +1,24 @@
-import React from 'react'
+import {useState} from 'react'
+import Mensaje from './Mensaje';
 
-const NuevoPresupuesto = () => {
+const NuevoPresupuesto = ({presupuesto, setPresupuesto}) => {
+
+    const [mensaje, setMensaje] = useState('');
+
+    const handlePresupuesto = e => {
+        e.preventDefault();
+
+        if(!Number(presupuesto) || Number(presupuesto) < 0){
+            setMensaje('No es un presupuesto válido')
+        } else {
+            setMensaje('Es un presupuesto válido')
+        }
+    }
+
   return (
     <div className='contenedor-presupuesto contenedor sombra'>
         
-        <form className='formulario'>
+        <form onSubmit={handlePresupuesto} className='formulario'>
             <div className='campo'>
                 <label>Definir presupuesto</label>
 
@@ -12,10 +26,14 @@ const NuevoPresupuesto = () => {
                 className='nuevo-presupuesto'
                 type='text'
                 placeholder='Añade tu presupuesto'
+                value={presupuesto}
+                onChange={ e => setPresupuesto(e.target.value)}
                 />
             </div>
 
             <input type="submit" value="Añadir" />
+
+            {mensaje && <Mensaje tipo="error">{mensaje}</Mensaje>}
         </form>
     </div>
   )

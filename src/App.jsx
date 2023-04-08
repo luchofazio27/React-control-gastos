81419e19c4b8a2295ddc8184701ad5273d7fa320
luchofazio27 <luchofazio@gmail.com>
2023-04-08 14:41:07 -0300

    Agregando icono de nuevo presupuesto

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,16 @@
 import { useState } from "react";
 import Header from "./components/Header";
+import IconoNuevoGasto from "./img/nuevo-gasto.svg";
 
 function App() {
   const [presupuesto, setPresupuesto] = useState(0);
   const [isValidPresupuesto, setIsValidPresupuesto] = useState(false);
+
+  const [modal, setModal] = useState(false)
+
+  const handleNuevoGasto = () => {
+    setModal(true)
+  }
 
   return (
     <div>
@@ -13,6 +20,15 @@ function App() {
         isValidPresupuesto={isValidPresupuesto}
         setIsValidPresupuesto={setIsValidPresupuesto}
       />
+
+      {isValidPresupuesto && (
+        <div className="nuevo-gasto">
+          <img src={IconoNuevoGasto} alt="icono nuevo gasto" onClick={handleNuevoGasto} />
+        </div>
+      )}
+
+{modal && <p>Desde modal</p>}
+
     </div>
   );
 }

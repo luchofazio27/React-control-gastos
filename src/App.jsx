e27d2de61d92c88735ba7fe6ff0583fc493a428d
luchofazio27 <luchofazio@gmail.com>
2023-04-11 22:15:40 -0300

    validando formulario Modal

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,7 @@
 import { useState } from "react";
 import Header from "./components/Header";
 import Modal from "./components/Modal";
+import { generarID } from "./helpers";
 import IconoNuevoGasto from "./img/nuevo-gasto.svg";
 
 function App() {
@@ -10,11 +11,23 @@ function App() {
   const [modal, setModal] = useState(false)
   const [animarModal, setAnimarModal] = useState(false)
 
+  const [gastos, setGastos] = useState([])
+
   const handleNuevoGasto = () => {
     setModal(true)
 
     setTimeout(() => {
       setAnimarModal(true)
+    }, 500);
+  }
+
+  const guardarGasto = gasto => {
+    gasto.id = generarID();
+    setGastos([...gastos, gasto])
+
+    setAnimarModal(false);
+    setTimeout(() => {
+      setModal(false);
     }, 500);
   }
 
@@ -37,6 +50,7 @@ function App() {
 setModal = {setModal}
 animarModal = {animarModal}
 setAnimarModal = {setAnimarModal}
+guardarGasto = {guardarGasto}
 />}
 
     </div>
--- src/components/Modal.jsx
+++ src/components/Modal.jsx
@@ -1,14 +1,33 @@
+import { useState } from "react";
+import Mensaje from "./Mensaje";
 import CerrarBtn from "../img/cerrar.svg";
 
-const Modal = ({ setModal, animarModal, setAnimarModal}) => {
+const Modal = ({ setModal, animarModal, setAnimarModal, guardarGasto }) => {
+  const [mensaje, setMensaje] = useState("");
+  const [nombre, setNombre] = useState("");
+  const [cantidad, setCantidad] = useState("");
+  const [categoria, setCategoria] = useState("");
 
   const ocultarModal = () => {
-    
     setAnimarModal(false);
-
     setTimeout(() => {
-        setModal(false);
+      setModal(false);
     }, 500);
+  };
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+
+    if ([nombre, cantidad, categoria].includes("")) {
+      setMensaje("Todos los campos son obligatorios");
+
+      setTimeout(() => {
+        setMensaje("");
+      }, 3000);
+      return;
+    }
+
+    guardarGasto({ nombre, cantidad, categoria });
   };
 
   return (
@@ -17,8 +36,57 @@ const Modal = ({ setModal, animarModal, setAnimarModal}) => {
         <img src={CerrarBtn} alt="cerrar modal" onClick={ocultarModal} />
       </div>
 
-      <form className={`formulario ${animarModal ? "animar" : "cerrar"}`}>
+      <form
+        onSubmit={handleSubmit}
+        className={`formulario ${animarModal ? "animar" : "cerrar"}`}
+      >
         <legend>Nuevo gasto</legend>
+        {mensaje && <Mensaje tipo="error">{mensaje}</Mensaje>}
+
+        <div className="campo">
+          <label htmlFor="nombre">Nombre Gasto</label>
+
+          <input
+            id="nombre"
+            type="text"
+            placeholder="Añade el Nombre del Gasto"
+            value={nombre}
+            onChange={(e) => setNombre(e.target.value)}
+          />
+        </div>
+
+        <div className="campo">
+          <label htmlFor="cantidad">Cantidad</label>
+
+          <input
+            id="cantidad"
+            type="number"
+            placeholder="Añade la cantidad del gasto: ej.30"
+            value={cantidad}
+            onChange={(e) => setCantidad(Number(e.target.value))}
+          />
+        </div>
+
+        <div className="campo">
+          <label htmlFor="categoria">Categoría</label>
+
+          <select
+            id="categoria"
+            value={categoria}
+            onChange={(e) => setCategoria(e.target.value)}
+          >
+            <option value="">-- Seleccione --</option>
+            <option value="ahorro">Ahorro</option>
+            <option value="comida">Comida</option>
+            <option value="casa">Casa</option>
+            <option value="gastos">Gastos Varios</option>
+            <option value="ocio">Ocio</option>
+            <option value="salud">Salud</option>
+            <option value="suscripciones">Suscripciones</option>
+          </select>
+        </div>
+
+        <input type="submit" value="Añadir Gasto" />
       </form>
     </div>
   );
